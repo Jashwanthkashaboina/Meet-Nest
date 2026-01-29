444f1959c38ab4aa9472e3cebae414c935d357c0
Jashwanthkashaboina <jashwanthkashaboina02@gmail.com>
2026-01-29 08:27:21 +0530

    Add frontend url to cors

--- backend/controllers/socketManager.js
+++ backend/controllers/socketManager.js
@@ -1,7 +1,12 @@
 const { Server } = require("socket.io");
 
 const connectToSocket = (server) => {
-  const io = new Server(server);
+  const io = new Server(server, {
+    cors: {
+      origin:  "http://localhost:5173",
+      methods: ["GET", "POST"],
+    }
+  });
 
   io.on("connection", (socket) => {
     console.log("User connected:", socket.id);
