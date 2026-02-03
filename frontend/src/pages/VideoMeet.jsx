51ac0113925b5ad87bd5c97c8d89faa44723753a
Jashwanthkashaboina <jashwanthkashaboina02@gmail.com>
2026-02-03 23:54:24 +0530

    Final Commit

--- frontend/src/pages/VideoMeet.jsx
+++ frontend/src/pages/VideoMeet.jsx
@@ -13,7 +13,7 @@ import ChatIcon from '@mui/icons-material/Chat'
 // import server from '../environment';
 import { useNavigate } from 'react-router';
 import withAuth from '../utils/WithAuth';
-import server from '../Environment.js';
+import server from '../../environment';
 
 const server_url = server;
 
